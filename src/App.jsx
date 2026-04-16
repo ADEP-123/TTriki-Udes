e38bd57411566f3e7423f035db18404aeeffe362
Andres Elizalde <andep210399@gmail.com>
2026-04-15 19:52:29 -0700

    feat: :sparkles: ensamblando juego en el index

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
+import Board from "./components/Board";
+import StatusBar from "./components/StatusBar";
+import ScoreBoard from "./components/ScoreBoard";
+import { useGameLogic } from "./hooks/useGameLogic";
+
 function App() {
+  const {
+    squares,
+    isXTurn,
+    winner,
+    winLine,
+    isDraw,
+    score,
+    handleClick,
+    resetGame,
+  } = useGameLogic();
+
   return (
     <div className="app">
-      <h1>TRIKI</h1>
-      <p>Próximamente...</p>
+      <h1 className="app__title">TRIKI</h1>
+      <ScoreBoard score={score} />
+      <StatusBar
+        winner={winner}
+        isDraw={isDraw}
+        isXTurn={isXTurn}
+        onReset={resetGame}
+      />
+      <Board squares={squares} winLine={winLine} onSquareClick={handleClick} />
     </div>
   );
 }
